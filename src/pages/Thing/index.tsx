70ad2ce4087b41f731e2683b02969cf50f0132db
zhengguodong <571954027@qq.com>
2022-08-29 22:02:17 +0800

    test

--- src/pages/Thing/index.tsx
+++ src/pages/Thing/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import thing from "./index.module.less"
+import type { CheckboxValueType } from 'antd/es/checkbox/Group';
+import {Checkbox} from "antd"
 function index() {
+  const plainOptions = ['Apple', 'Pear', 'Orange'];
+  const onChange = (checkedValues: CheckboxValueType[]) => {
+    console.log('checked = ', checkedValues);
+  };
   return (
-    <div>index</div>
+    <div>
+      <div className={thing.box}>
+        <h3 className={thing.title}>第三方包和插件的使用</h3>
+        <div className={thing.item1}> 
+          <Checkbox.Group options={plainOptions} onChange={onChange} className={thing.antd_checkbox}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
