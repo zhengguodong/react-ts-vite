af31a6171d63e78fa691e6433fc48e8307e497bf
zhengguodong <571954027@qq.com>
2022-09-30 11:10:38 +0800

    tt

--- src/pages/Article_up/index.tsx
+++ src/pages/Article_up/index.tsx
@@ -4,21 +4,26 @@ import {Input,Button, message} from "antd"
 import {useNavigate} from "react-router-dom"
 function index() {
     let navigate=useNavigate()
-    const [value,setValue]=useState<any>('')
+    const [valueContent,setValueContent]=useState<string>('')
     const [title,setTitle]=useState<string>('')
     let back=()=>{
         navigate("/home")
         message.info("取消成功")
+    }
+    let changContent = (e:any) => {
+        setValueContent(e.target.value)
     }
     return (
         <div>
             <Input placeholder="请输入文章标题" value={title} onInput={(e:any)=>{setTitle(e.target.value)}}  maxLength={20} bordered={false} style={{"width":"50%","height":"50px"}}/>
             <div style={{"display":"inline-block","float":"right","lineHeight":"50px","marginRight":"10px"}}>
                 <Button type="primary" style={{"marginRight":"10px"}}>发布</Button>
-                <Button onClick={()=>back()}>取消</Button> 
+                <Button onClick={()=>back()}>取消</Button>
             </div>
-           
-            <Editor value={value} onChange={(e:any) =>setValue(e.target.value)} style={{"width":"100%","height":"calc(100vh - 50px)"}}></Editor>
+            <div>
+                <Editor value={valueContent} onChange={(e:any) =>changContent(e)} style={{"width":"100%","height":"calc(100vh - 50px)"}}></Editor>
+            </div>
+
         </div>
     )
 }
